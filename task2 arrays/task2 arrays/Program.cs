d881ff84409db4725b09bad463c0d09a44819908
Olga <o.a.popova98@gmail.com>
2018-11-11 04:28:44 +0300

    Task 2. Arrays. Version 1.

---
+++ task2 arrays/task2 arrays/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[3, 2];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Write the " + "[" + i + j + "]" + " element of array: ");
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            int[,] arr2 = new int[3, 2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Write the " + "[" + i + j + "]" + " element of array: ");
+                    arr2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            int[,] arr3 = new int[3, 2];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr3[i, j] = arr1[i, j] + arr2[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("The first array is:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("The second array is:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("The final array is:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr3[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
