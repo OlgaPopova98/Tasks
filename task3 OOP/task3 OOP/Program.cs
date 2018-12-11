bca4edf2a15a9e77b2c38de0103baba4b438ef87
Olga <o.a.popova98@gmail.com>
2018-12-11 18:38:59 +0300

    Add Collectoin

--- task3 OOP/task3 OOP/Program.cs
+++ task3 OOP/task3 OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,38 @@ namespace task3_OOP
     {
         static void Main(string[] args)
         {
+            
             Dog Kollie = new Dog(9.33f, "Tom");
             Console.WriteLine(Kollie.Name);
             Console.WriteLine();
             Dog Labrador = new Dog(10.23f, "Prince");
             Labrador.Print();
             Console.WriteLine();
+            Dog Dalmatian = new Dog(10.23f, "Perry");
+            Dalmatian.Print();
+            Console.WriteLine();
             Cat Persian = new Cat(8.5f, "Peach");
             Persian.Print();
+            Console.WriteLine();
+            Cat Scottish = new Cat(8.5f, "Grant");
+            Scottish.Print();
+
+            List<string> animals = new List<string> { };
+            animals.Add(Kollie.Name);
+            animals.Add(Labrador.Name);
+            animals.Add(Dalmatian.Name);
+            animals.Add(Persian.Name);
+            animals.Add(Scottish.Name);
+            Console.WriteLine();
+            Console.WriteLine("Number of elements: " + animals.Count);
+            foreach (string element in animals)
+                Console.WriteLine(element);
+            animals.Sort();
+            Console.WriteLine();
+            Console.WriteLine("Sorted elements: ");
+            foreach (string element in animals)
+                Console.WriteLine(element);
+
 
 
             Console.ReadKey();
